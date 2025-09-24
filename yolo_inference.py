9c1d0b399bed8b3dfd81adb0026c0318c8723446
Inshalmunaf <inshalmunaf@gmail.com>
2025-09-24 19:59:17 +0500

    Object tracking comppleted

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')
+model = YOLO('yolov8x.pt')
 
-result = model.predict('input_videos/input_video.mp4' , save = True )
+result = model.track('input_videos/input_video.mp4' , conf = 0.2 ,save = True )
 
-for box in result[0].boxes:
-    print(box)
+# for box in result[0].boxes:
+# print(box)
